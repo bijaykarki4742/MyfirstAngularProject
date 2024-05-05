34c6e0810091fd3616cfbbe47034c08b02e1f9cf
bijay karki <bijaykarki4742@gmail.com>
2024-05-05 17:21:06 +0545

    json file added and added section b and section c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,18 @@ import { SectionfComponent } from './sectionf/sectionf.component';
 import { SectiongComponent } from './sectiong/sectiong.component';
 import { SectionhComponent } from './sectionh/sectionh.component';
 import { SectioniComponent } from './sectioni/sectioni.component';
+import {SectionbComponent} from "./sectionb/sectionb.component";
+import {SectioncComponent} from "./sectionc/sectionc.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,InstructionComponent,SectionaComponent, SectiondComponent, SectioneComponent, SectionfComponent, SectiongComponent, SectionhComponent, SectioniComponent],
+  imports: [RouterOutlet, InstructionComponent,
+    SectionaComponent,SectionbComponent,
+    SectioncComponent ,SectiondComponent,
+    SectioneComponent, SectionfComponent,
+    SectiongComponent, SectionhComponent,
+    SectioniComponent, SectionbComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
